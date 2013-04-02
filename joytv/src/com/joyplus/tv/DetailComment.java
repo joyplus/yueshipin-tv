2c51bc09b6d412df4fe574fd35b95bef33186934
YuMinghe <veteranyu@163.com>
2013-04-02 21:40:28 +0800

    yu

--- joytv/src/com/joyplus/tv/DetailComment.java
+++ joytv/src/com/joyplus/tv/DetailComment.java
@@ -2,22 +2,32 @@ package com.joyplus.tv;
 
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Iterator;
+import java.util.List;
 
-import org.json.JSONException;
 import org.json.JSONObject;
 
 import android.app.Activity;
 import android.content.ActivityNotFoundException;
+import android.content.Context;
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.os.Handler;
 import android.util.Log;
+import android.view.KeyEvent;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
+import android.webkit.URLUtil;
 import android.widget.AbsListView;
 import android.widget.AbsListView.OnScrollListener;
 import android.widget.AdapterView;
+import android.widget.BaseAdapter;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import com.androidquery.AQuery;
 import com.androidquery.callback.AjaxCallback;
@@ -25,7 +35,7 @@ import com.androidquery.callback.AjaxStatus;
 import com.fasterxml.jackson.core.JsonParseException;
 import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.joyplus.tv.Adapters.DetailCommentListAdapter;
+import com.joyplus.tv.R;
 import com.joyplus.tv.Adapters.DetailCommentListData;
 import com.joyplus.tv.Service.Return.ReturnProgramReviews;
 import com.parse.Parse;
@@ -33,7 +43,7 @@ import com.umeng.analytics.MobclickAgent;
 import com.umeng.update.UmengUpdateAgent;
 
 public class DetailComment extends Activity implements
-		android.widget.AdapterView.OnItemClickListener {
+		android.widget.AdapterView.OnItemClickListener{
 	private String TAG = "DetailComment";
 	private AQuery aq;
 	private App app;
@@ -48,6 +58,8 @@ public class DetailComment extends Activity implements
 	private ListView ItemsListView;
 	private DetailCommentListAdapter DetailCommentAdapter;
 	private int isLastisNext = 1;
+	private boolean isDetailComment = false;
+	private int CurrentIndex = 0;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -61,6 +73,20 @@ public class DetailComment extends Activity implements
 		ItemsListView = (ListView) findViewById(R.id.listView1);
 		// 设置listview的点击事件监听器
 		ItemsListView.setOnItemClickListener(this);
+		
+		ItemsListView.setOnKeyListener(new View.OnKeyListener() {
+				
+				@Override
+				public boolean onKey(View v, int keyCode, KeyEvent event) {
+					// TODO Auto-generated method stub
+					int action = event.getAction();
+					if (action == KeyEvent.ACTION_DOWN && keyCode == KeyEvent.KEYCODE_NUMPAD_5) {
+						aq.id(R.id.scrollViewItemDetail).gone();
+						aq.id(R.id.listView1).visible ();
+					}
+					return false;
+				}
+			});
 		ItemsListView.setOnScrollListener(new OnScrollListener() {
 			@Override
 			public void onScrollStateChanged(AbsListView view, int scrollState) {
@@ -86,16 +112,40 @@ public class DetailComment extends Activity implements
 //		prod_id = intent.getStringExtra("prod_id");
 //		prod_name = intent.getStringExtra("prod_name");
 		
-		prod_id = "1004572";
-		prod_name = "上位";
-		prod_dou = "3";
-		prod_url = "http://img.funshion.com/pictures/159/820/7/1598207.jpg";
+		prod_id = "6524";
+		prod_name = "那年冬天,风在吹";
+		prod_dou = "3.5";
+		prod_url = "http://img3.douban.com/view/photo/photo/public/p1869602430.jpg";
 		
+		aq.id(R.id.textView_dou).text(prod_dou);
+		aq.id(R.id.textView1).text(prod_name);
+		aq.id(R.id.imageViewBarCode).image(prod_url, true,
+				true, 0, R.drawable.movie_pic);
+
 		if (prod_id != null)
 			CheckSaveData();
+		if(Float.parseFloat(prod_dou) >0)
+			InitDou();
 		// MobclickAgent.setDebugMode(true);
 	}
-
+	public void InitDou() {
+		String m_j = null;
+		int i = 0;
+		Float f_dou = Float.parseFloat(prod_dou);
+		
+		for (i = 0; i < f_dou/1; i++) {
+			m_j = Integer.toString(i + 2);
+			ImageView m_ImageView = (ImageView) this.findViewById(getResources()
+					.getIdentifier("imageView" + m_j, "id", getPackageName()));
+			m_ImageView.setImageResource(R.drawable.star_on);
+		}
+		if(f_dou%i > 0){
+			m_j = Integer.toString(i+1);
+			ImageView m_ImageView = (ImageView) this.findViewById(getResources()
+					.getIdentifier("imageView" + m_j, "id", getPackageName()));
+			m_ImageView.setImageResource(R.drawable.star_half);
+		}
+	}
 	public void OnClickTab1TopLeft(View v) {
 
 	}
@@ -136,12 +186,17 @@ public class DetailComment extends Activity implements
 
 		if (m_ReturnProgramReviews.reviews == null)
 			return;
+		
+		aq.id(R.id.scrollViewItemDetail).gone();
+		aq.id(R.id.listView1).visible();
+		
+		
 		if (isLastisNext > 1) {
 			for (int i = 0; i < m_ReturnProgramReviews.reviews.length; i++) {
 				DetailCommentListData m_DetailCommentListData = new DetailCommentListData();
 				m_DetailCommentListData.Prod_ID = m_ReturnProgramReviews.reviews[i].review_id;
 				m_DetailCommentListData.Prod_title = m_ReturnProgramReviews.reviews[i].title;
-				m_DetailCommentListData.Prod_comments = m_ReturnProgramReviews.reviews[i].comments;
+				m_DetailCommentListData.Prod_comments = m_ReturnProgramReviews.reviews[i].comments.replace("\r", "\r\n");
 				dataStruct.add(m_DetailCommentListData);
 				DetailCommentAdapter.notifyDataSetChanged();
 			}
@@ -149,17 +204,17 @@ public class DetailComment extends Activity implements
 			
 		} else
 		{
-			NotifyDataAnalysisFinished();
+			DetailCommentAdapter.notifyDataSetChanged();
 		}
 		
 		for (int i = 0; i < m_ReturnProgramReviews.reviews.length; i++) {
 			DetailCommentListData m_DetailCommentListData = new DetailCommentListData();
 			m_DetailCommentListData.Prod_ID = m_ReturnProgramReviews.reviews[i].review_id;
 			m_DetailCommentListData.Prod_title = m_ReturnProgramReviews.reviews[i].title;
-			m_DetailCommentListData.Prod_comments = m_ReturnProgramReviews.reviews[i].comments;
+			m_DetailCommentListData.Prod_comments = m_ReturnProgramReviews.reviews[i].comments.replace("\r", "\r\n");
 			dataStruct.add(m_DetailCommentListData);
 		}
-
+		DetailCommentAdapter.notifyDataSetChanged();
 	}
 
 	public void OnClickImageView(View v) {
@@ -191,6 +246,11 @@ public class DetailComment extends Activity implements
 						json.toString());
 			// 创建数据源对象
 			GetReviews();
+			if (dataStruct.size() > 0) {
+				aq.id(R.id.textView3).text(
+						"1/" + Integer.toString(dataStruct.size()));
+				aq.id(R.id.textView3).visible();
+			}
 //			aq.id(R.id.ProgressText).gone();
 		} catch (JsonParseException e) {
 			// TODO Auto-generated catch block
@@ -205,47 +265,19 @@ public class DetailComment extends Activity implements
 
 	}
 
-	// 数据更新
-	public void NotifyDataAnalysisFinished() {
-		if (dataStruct != null && ItemsListView != null) {
-			DetailCommentListAdapter listviewdetailadapter = getAdapter();
-			ItemsListView.setAdapter(listviewdetailadapter);
-		} else {
-			app.MyToast(this, "ItemsListView empty.");
-		}
-	}
-
-	private DetailCommentListAdapter getAdapter() {
-		if (DetailCommentAdapter == null) {
-			ArrayList arraylist = dataStruct;
-			DetailCommentListAdapter listviewdetailadapter = new DetailCommentListAdapter(this,
-					arraylist);
-			DetailCommentAdapter = listviewdetailadapter;
-		} else {
-			ArrayList arraylist1 = dataStruct;
-			DetailCommentListAdapter listviewdetailadapter1 = new DetailCommentListAdapter(this,
-					arraylist1);
-			DetailCommentAdapter = listviewdetailadapter1;
-		}
-		return DetailCommentAdapter;
-	}
-
 	// listview的点击事件接口函数
 	@Override
 	public void onItemClick(AdapterView adapterview, View view, int i, long l) {
+		CurrentIndex = i;
 		DetailCommentListData m_DetailCommentListData = (DetailCommentListData) ItemsListView
 				.getItemAtPosition(i);
 		if (m_ReturnProgramReviews != null) {
-			// app.MyToast(this, m_DetailCommentListData.Pic_name, Toast.LENGTH_LONG)
-			// .show();
-//			Intent intent = new Intent(this, Detail_BangDan.class);
-//			intent.putExtra("BangDan_id", m_DetailCommentListData.Pic_ID);
-//			intent.putExtra("BangDan_name", m_DetailCommentListData.Pic_name);
-//			try {
-//				startActivity(intent);
-//			} catch (ActivityNotFoundException ex) {
-//				Log.e(TAG, "Call Detail_BangDan failed", ex);
-//			}
+			isDetailComment = true;
+			aq.id(R.id.listViewItemTitle).text(m_DetailCommentListData.Prod_title);
+			aq.id(R.id.listViewItemDetail).text(m_DetailCommentListData.Prod_comments);
+			aq.id(R.id.listView1).gone();
+			aq.id(R.id.scrollViewItemDetail).visible();
+
 		} else {
 			app.MyToast(this, "ReturnProgramReviews is empty.");
 		}
@@ -256,6 +288,10 @@ public class DetailComment extends Activity implements
 		String SaveData = null;
 		ObjectMapper mapper = new ObjectMapper();
 		SaveData = app.GetServiceData("DetailComment" +prod_id +Integer.toString(isLastisNext));
+		
+		dataStruct = new ArrayList();
+		DetailCommentAdapter = new DetailCommentListAdapter();
+		ItemsListView.setAdapter(DetailCommentAdapter);
 		if (SaveData == null) {
 			GetServiceData(isLastisNext);
 		} else {
@@ -287,8 +323,8 @@ public class DetailComment extends Activity implements
 	}
 
 	public void GetServiceData(int index) {
-		String url = Constant.BASE_URL + "program/reviews" +"?prod_id=" + prod_id +"page_num="
-				+ Integer.toString(index) + "&page_size=10";
+		String url = Constant.BASE_URL + "program/reviews" +"?prod_id=" + prod_id +"&page_num="
+				+ Integer.toString(index) + "&page_size=6";
 
 		AjaxCallback<JSONObject> cb = new AjaxCallback<JSONObject>();
 		cb.url(url).type(JSONObject.class).weakHandler(this, "InitListData");
@@ -299,4 +335,78 @@ public class DetailComment extends Activity implements
 		aq.ajax(cb);
 
 	}
+	@Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+           if (keyCode == KeyEvent.KEYCODE_BACK && isDetailComment){                    
+        	   	aq.id(R.id.scrollViewItemDetail).gone();
+   				aq.id(R.id.listView1).visible ();
+   				DetailCommentAdapter.notifyDataSetChanged();
+   				isDetailComment = false;
+                 return true;
+           }
+           return super.onKeyDown(keyCode, event);
+    }
+	static class ViewHolder {
+		ImageView imageView1;
+		TextView textView01;
+		TextView textView02;
+	}
+	public class DetailCommentListAdapter extends BaseAdapter {
+
+		@Override
+		public int getCount() {
+			return dataStruct.size();
+		}
+
+		@Override
+		public DetailCommentListData getItem(int position) {
+			return (DetailCommentListData) dataStruct.get(position);
+		}
+
+		@Override
+		public long getItemId(int position) {
+			return position;
+		}
+
+		@Override
+		public View getView(int position, View convertView, ViewGroup viewGroup) {
+			
+			DetailCommentListData m_DetailCommentListData = (DetailCommentListData) getItem(position);
+			ViewHolder holder;
+			
+			if (convertView == null) {
+				convertView = getLayoutInflater().inflate(
+						R.layout.detail_comment_list, viewGroup, false);
+
+				holder = new ViewHolder();
+				
+				holder.imageView1 = (ImageView) convertView
+						.findViewById(R.id.imageView1);
+				
+				holder.textView01 = (TextView) convertView
+						.findViewById(R.id.TextView01);
+				
+				holder.textView02 = (TextView) convertView
+						.findViewById(R.id.TextView02);
+
+				convertView.setTag(holder);
+
+			} else {
+				holder = (ViewHolder) convertView.getTag();
+			}
+			if (CurrentIndex == position) {
+				holder.imageView1.setVisibility(View.VISIBLE);
+				convertView.setBackgroundResource(R.drawable.bg_teat_repeat);
+			} else {
+				holder.imageView1.setVisibility(View.INVISIBLE);
+				convertView.setBackgroundResource(0);
+			}
+
+			holder.textView01.setText(m_DetailCommentListData.Prod_title);
+			holder.textView02.setText(m_DetailCommentListData.Prod_comments);
+
+			return convertView;
+		}
+	}
+	
 }
