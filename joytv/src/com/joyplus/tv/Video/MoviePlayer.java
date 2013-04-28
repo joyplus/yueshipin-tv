3b4560936be2c253aab209607e24ec8796886e19
YuMinghe <veteranyu@163.com>
2013-04-28 17:25:49 +0800

    yu

--- joytv/src/com/joyplus/tv/Video/MoviePlayer.java
+++ joytv/src/com/joyplus/tv/Video/MoviePlayer.java
@@ -916,7 +916,8 @@ public class MoviePlayer implements MediaPlayer.OnErrorListener,
 				|| keyCode == KeyEvent.KEYCODE_DPAD_LEFT
 				|| keyCode == KeyEvent.KEYCODE_DPAD_RIGHT
 				|| keyCode == KeyEvent.KEYCODE_DPAD_DOWN
-				|| keyCode == KeyEvent.KEYCODE_DPAD_UP;
+				|| keyCode == KeyEvent.KEYCODE_DPAD_UP
+				|| keyCode == KeyEvent.KEYCODE_DPAD_CENTER;
 	}
 
 	// VOLUME
