bc3736cbd666accc8d74e3ed62e45f9d154c7e54
yanyuchuang <751523479@qq.com>
2013-06-20 12:43:44 +0800

    yy

--- joytv/src/com/joyplus/tv/Video/VideoPlayerActivity.java
+++ joytv/src/com/joyplus/tv/Video/VideoPlayerActivity.java
@@ -112,7 +112,7 @@ public class VideoPlayerActivity extends Activity  implements AdListener{
 	private RelativeLayout layout;
 	private AdView mAdView;
 	private String publisherId = "b7a59bdb75c2e6341e6dfac70b68b709";//要显示广告的publisherId
-	private boolean animation = false;//该广告加载时是否用动画效果
+	private boolean animation = true;//该广告加载时是否用动画效果
 	
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
