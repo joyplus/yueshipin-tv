86af68d82a5c91200f433f918545dc5989bb8192
zhengang <zhengang.yang@joyplus.tv>
2013-06-26 18:59:22 +0800

    zhengang updatecode

--- joytv/src/com/joyplus/tv/VideoPlayerJPActivity.java
+++ joytv/src/com/joyplus/tv/VideoPlayerJPActivity.java
@@ -1,12 +1,22 @@
 package com.joyplus.tv;
 
 import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URISyntaxException;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import org.apache.http.Header;
+import org.apache.http.HttpResponse;
+import org.apache.http.HttpStatus;
+import org.apache.http.StatusLine;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.params.HttpConnectionParams;
+import org.apache.http.params.HttpParams;
 import org.json.JSONException;
 import org.json.JSONObject;
 import org.jsoup.Jsoup;
@@ -26,6 +36,8 @@ import android.media.AudioManager;
 import android.media.MediaPlayer;
 import android.net.TrafficStats;
 import android.net.Uri;
+import android.net.http.AndroidHttpClient;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
@@ -1725,6 +1737,128 @@ public class VideoPlayerJPActivity extends Activity implements
 				}
 			}
 		}
+	}
+	
+	/**
+	 * 地址跳转
+	 */
+	
+	private void urlRedirectTask() {
+		
+		new AsyncTask<Void, Void, Void>() {
+
+			@Override
+			protected Void doInBackground(Void... params) {
+				// TODO Auto-generated method stub
+				
+				String urlStr = null;
+				while(urlStr == null) {
+					
+					List<String> list = new ArrayList<String>();
+					
+					try {
+						urlRedirect(currentPlayUrl,list);
+					} catch (Exception e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+						//超时异常
+					}
+					
+					if(list.size() > 0) {
+						
+						 urlStr = list.get(list.size() -1);
+						if(urlStr != null) {
+							
+							if(URLUtil.isHttpUrl(urlStr)) {
+								
+								
+								break;
+							}
+						}
+					}
+				}
+
+				return null;
+			}
+		}.equals(null);
+	}
+	
+	private void urlRedirect(String urlStr,List<String> list) {
+		
+		// 模拟火狐ios发用请求 使用userAgent
+		AndroidHttpClient mAndroidHttpClient = AndroidHttpClient
+				.newInstance(Constant.USER_AGENT_IOS);
+
+		HttpParams httpParams = mAndroidHttpClient.getParams();
+		// 连接时间最长5秒，可以更改
+		HttpConnectionParams.setConnectionTimeout(httpParams, 5000 * 1);
+		
+		URL url;
+		try {
+			url = new URL(urlStr);
+			HttpGet mHttpGet = new HttpGet(url.toURI());
+			HttpResponse response = mAndroidHttpClient.execute(mHttpGet);
+			StatusLine statusLine = response.getStatusLine();
+			
+			int status = statusLine.getStatusCode();
+			Log.i(TAG, "HTTP STATUS : " + status);
+			
+			if (status == HttpStatus.SC_OK) {
+				Log.i(TAG, "HttpStatus.SC_OK--->");
+				// 正确的话直接返回，不进行下面的步骤
+				mAndroidHttpClient.close();
+				list.add(urlStr);
+				
+				return;//后面不执行
+			} else {
+				
+				Log.i(TAG, "NOT HttpStatus.SC_OK--->");
+				
+				if (status == HttpStatus.SC_MOVED_PERMANENTLY || // 网址被永久移除
+						status == HttpStatus.SC_MOVED_TEMPORARILY || // 网址暂时性移除
+						status == HttpStatus.SC_SEE_OTHER || // 重新定位资源
+						status == HttpStatus.SC_TEMPORARY_REDIRECT) {// 暂时定向
+					
+					Header header = response.getFirstHeader("Location");// 拿到重新定位后的header
+					
+					if(header != null) {
+						
+						String location = header.getValue();// 从header重新取出信息
+						Log.i(TAG, "Location: " + location);
+						if(location != null && location.equals("")) {
+							
+							urlRedirect(location, list);
+							
+							mAndroidHttpClient.close();// 关闭此次连接
+							return;//后面不执行
+						}
+					}
+					
+					list.add(null);
+					mAndroidHttpClient.close();
+					
+					return;
+
+				} else {//地址真的不存在
+					
+					mAndroidHttpClient.close();
+					list.add(null);
+					
+					return;//后面不执行
+				}
+			}
+		} catch (MalformedURLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (URISyntaxException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		
 	}
 	
 }
