381c0fa12b69cd2ee889622a7743580ad5e41e39
zhengang <zhengang.yang@joyplus.tv>
2013-09-16 15:43:30 +0800

    zhengang add p2p retry

--- joytv/src/com/joyplus/tv/entity/URLS_INDEX.java
+++ joytv/src/com/joyplus/tv/entity/URLS_INDEX.java
@@ -1,21 +1,23 @@
 package com.joyplus.tv.entity;
-public class URLS_INDEX{
-//		public int souces;
-//		public int defination;
-//		public String url;
-//		public String source_from;
-		
-		public int souces;
-		public int defination;
-		public String url;
-		public String source_from;
-		public String defination_from_server;
-		
-		@Override
-		public String toString() {
+
+public class URLS_INDEX {
+
+	public int souces;// 来源 例如P2P 本地排序使用
+	public int defination;// 清晰度优先级 本地排序使用
+	public String url;// 播放地址
+	public String bakUrl;//重试临时保存地址
+	public String source_from;// 来源 例如P2P 网络获取
+	public String defination_from_server;// 清晰度 例如HD2
+
+	@Override
+	public String toString() {
 		// TODO Auto-generated method stub
-		return "souces:" + souces + " defination:" + defination + " url" + url
-				+ " source_from:" + source_from + "  defination_from_server:" + defination_from_server;
-		}
-		
+		return "souces:" + souces + 
+				" defination:" + defination + 
+				" url" + url+ 
+				" bakUrl" + bakUrl+ 
+				" source_from:" + source_from + 
+				"  defination_from_server:"+ defination_from_server;
 	}
+
+}
