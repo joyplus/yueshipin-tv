d2d6d57e66c6b1f764c1e1ebb68ae8c460ed7c2f
zhengang <zhengang.yang@joyplus.tv>
2013-11-01 18:43:42 +0800

    zhengang fix null point bug

--- JoyplusSubtitle/JoyplusSubtitle/src/com/joyplus/sub/JoyplusSubCarrierContentRestriction.java
+++ JoyplusSubtitle/JoyplusSubtitle/src/com/joyplus/sub/JoyplusSubCarrierContentRestriction.java
@@ -4,33 +4,40 @@ import com.joyplus.sub.JoyplusSubInterface.SubContentType;
 
 public class JoyplusSubCarrierContentRestriction implements JoyplusSubContentRestriction{
 	private final static int MAXSIZE = 500*1024;
+	private Object mObject=new Object();
 	@Override
 	public void checkSubSize(long SubSize, long increaseSize) throws ContentRestrictionException {
 		// TODO Auto-generated method stub
-		if(SubSize<0 || increaseSize<0){
-			throw new ContentRestrictionException();
-		}
-		if((SubSize+increaseSize)>MAXSIZE){
-			throw new ContentRestrictionException();
-		}
+		synchronized (mObject) {
+			if(SubSize<0 || increaseSize<0){
+				throw new ContentRestrictionException();
+			}
+			if((SubSize+increaseSize)>MAXSIZE){
+				throw new ContentRestrictionException();
+			}
+		}		
 	}
 	@Override
 	public void checkUri(SubContentType type, SubURI uri)
 			throws ContentRestrictionException {
 		// TODO Auto-generated method stub
-		if(type == null || uri==null || type==SubContentType.SUB_UNKNOW){
-			throw new ContentRestrictionException();
-		}
-		if(type == SubContentType.SUB_SRT && uri.SubType==SUBTYPE.NETWORK){
-			CheckSRTUri(uri.getUrl());
+		synchronized (mObject){
+			if(type == null || uri==null || type==SubContentType.SUB_UNKNOW){
+				throw new ContentRestrictionException();
+			}
+			if(type == SubContentType.SUB_SRT && uri.SubType==SUBTYPE.NETWORK){
+				CheckSRTUri(uri.getUrl());
+			}
 		}
 	}
 	private void CheckSRTUri(String uri) {
 		// TODO Auto-generated method stub
-		if(uri == null || "".equals(uri))throw new ContentRestrictionException();
-		if(uri.contains("scid=" ))return;
-		if(uri.length() == uri.indexOf(".srt") + 4)return;
-		throw new ContentRestrictionException();
+		synchronized (mObject){
+			if(uri == null || "".equals(uri))throw new ContentRestrictionException();
+			if(uri.contains("scid=" ))return;
+			if(uri.length() == uri.indexOf(".srt") + 4)return;
+			throw new ContentRestrictionException();
+		}
 	}
 
 	
