02633c89d0903f4280c1d1756c12b59ea1f1dbde
zhengang <zhengang.yang@joyplus.tv>
2013-11-04 10:53:34 +0800

    zhengang add detail outer interface

--- joytv/src/com/joyplus/tv/utils/ShowDtailConfig.java
+++ joytv/src/com/joyplus/tv/utils/ShowDtailConfig.java
@@ -1,29 +1,42 @@
 package com.joyplus.tv.utils;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import android.content.Intent;
 import android.net.Uri;
+import android.text.TextUtils;
 
+/**
+ * 参数文件，不可修改
+ * @author Joyplus
+ */
 public class ShowDtailConfig {
 
 	/**
-	 * intent.putExtra("ID", IDValue);
-	 * intent.putExtra("prod_url", prod_urlValue);
+	 * Example：
+	 * prod_id: "1016663" 
+	 * prod_name: "笔仙2" 
+	 * prod_type: "1" 
+	 * prod_pic_url:"http://img4.douban.com/view/photo/photo/public/p2023557318.jpg"
+	 * 
+	 * intent.putExtra("ID", IDValue); 
+	 * intent.putExtra("prod_url",prod_urlValue); 
 	 * intent.putExtra("prod_name", prod_nameValue);
-	 * intent.putExtra("stars", starsValue);
-	 * intent.putExtra("directors", directorsValue);
+	 * intent.putExtra("stars", starsValue); 
+	 * intent.putExtra("directors",directorsValue);
 	 * intent.putExtra("summary", summaryValue);
 	 * intent.putExtra("support_num", support_numValue);
 	 * intent.putExtra("favority_num", favority_numValue);
-	 * intent.putExtra("definition", definitionValue);
-	 * intent.putExtra("score", scoreValue);
+	 * intent.putExtra("definition", definitionValue); 
+	 * intent.putExtra("score",scoreValue);
 	 */
 	
 	// 影片唯一标是ID，必须传
 	public static final String ID 				= "ID";
-	// 影片播放地址，可不传
+	// 影片海报图片地址，可不传
 	public static final String PROD_URL 		= "prod_url";
 	// 影片播放名字，可不传
 	public static final String PROD_NAME 		= "prod_name";
@@ -42,6 +55,7 @@ public class ShowDtailConfig {
 	// 影片豆瓣评分，可不传
 	public static final String SCORE 			= "score";
 	
+	
 	//外部需要添加的Action
 	public static final String JOYPLUS_INTENT_ACTION 					= Intent.ACTION_VIEW;
 	//外部需要添加的Category 
@@ -55,6 +69,7 @@ public class ShowDtailConfig {
 	//外部进入电视剧详情
 	public static final String JOYPLUS_DATA_MIME_TYPE_VARIETY 		= "joyplus_data_mime_detail_variety/*";
 	
+	
 	//四种数据类型
 	private static final List<String> JOYPLUS_MIME_LISTS					 = new ArrayList<String>();
 	
@@ -65,8 +80,31 @@ public class ShowDtailConfig {
 		JOYPLUS_MIME_LISTS.add(JOYPLUS_DATA_MIME_TYPE_VARIETY);
 	}
 	
+	//电影类型
+	public static final String MOVIE_TYPE = "1";
+	//电视剧类型
+	public static final String TVSERIES_TYPE = "2";
+	//综艺类型
+	public static final String VARIETY_TYPE = "3";
+	//动漫类型
+	public static final String ANIME_TYPE = "131";
+	
+	private static final Map<String,String> JOYPLUS_MIME_MAPS						= new HashMap<String,String>();
+	static{
+		JOYPLUS_MIME_MAPS.put(MOVIE_TYPE, JOYPLUS_DATA_MIME_TYPE_MOVIE);
+		JOYPLUS_MIME_MAPS.put(TVSERIES_TYPE, JOYPLUS_DATA_MIME_TYPE_TVSERIES);
+		JOYPLUS_MIME_MAPS.put(VARIETY_TYPE, JOYPLUS_DATA_MIME_TYPE_VARIETY);
+		JOYPLUS_MIME_MAPS.put(ANIME_TYPE, JOYPLUS_DATA_MIME_TYPE_ANIME);
+	}
+	
+	public static Intent getIntent4VideoType(String type) throws Exception{
+		if(TextUtils.isEmpty(type) 
+				|| TextUtils.isEmpty(JOYPLUS_MIME_MAPS.get(type))) throw new Exception();
+		return getIntent4MimeType(JOYPLUS_MIME_MAPS.get(type));
+	}
+	
 	//获取JoyplusIntent
-	public static Intent getIntent4MimeType(String mimeType) throws Exception{
+	private static Intent getIntent4MimeType(String mimeType) throws Exception{
 		Intent intent = new Intent(ShowDtailConfig.JOYPLUS_INTENT_ACTION);
 		intent.addCategory(ShowDtailConfig.JOYPLUS_INTENT_CATEGORY);
 		if(ShowDtailConfig.JOYPLUS_MIME_LISTS.contains(mimeType)){
